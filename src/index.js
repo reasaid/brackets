89be153326b110193e32ba7096392185b1e26408
Maria Lobanova <roldik95@gmail.com>
2021-03-13 18:01:52 +0300

    feat: add function cheak

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+    let newArray = [];
+
+    for(let i = 0; i < bracketsConfig.length; i++){
+        newArray.push(bracketsConfig[i].join(''));
+    }
+
+     // Объявляем функцию, которая будет последовательность из newArray вырезать 
+
+    function cutter(string){
+        let cutStr = string;
+        for ( let i = 0; i<newArray.length; i++){
+            cutStr = cutStr.replace(newArray[i], "");
+        }
+        return string === cutStr ? cutStr : cutter(cutStr); // Создаем рекурсию пока  последовательность существует 
+    }
+
+    const result = cutter(str); // Вызов рекурсивной функции и записываем ее вывод в переменную result
+  
+    //Сравниваем  результат функции cutter, пустой ли он?
+ 
+   return result === "" ? true : false;
 }
